4a127aacee371058eb72c2a5fbe13d61309f7df4
jaysingtalekar268 <92983089+jaysingtalekar268@users.noreply.github.com>
2023-05-22 11:29:46 +0530

    added similar module

--- client/src/components/movie/movie.js
+++ client/src/components/movie/movie.js
@@ -2,18 +2,22 @@ import { useEffect, useRef, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { useLocation, useNavigate, Navigate } from "react-router-dom";
 import styles from "./movie.module.css";
-import { TextCenter } from "react-bootstrap-icons";
-
+import hstyles from "../home/home.module.css";
+import Page from "../page/page";
 
 
 function Movie() {
 
     const navigate = useNavigate();
     const [reviewData, setRData] = useState();
+    const [similarData, setSData] = useState();
+
+    const [sCounter, setSCounter] = useState(1);
 
     const location = useLocation();
 
-    const api_flag = useRef(true);
+    const rflag = useRef(true);
+    const sflag = useRef(true);
     const get_review = async () => {
 
         let reviewResult = await fetch(`https://api.themoviedb.org/3/movie/${location.state.selecredMovieData.id}/reviews?api_key=${process.env.REACT_APP_API_KEY}`, {
@@ -26,18 +30,65 @@ function Movie() {
         reviewResult = await reviewResult.json();
 
         if (reviewResult) {
-            api_flag.current = false;
-            // console.warn(reviewResult);
+            rflag.current = false;
             setRData(reviewResult);
+        }
+    }
+    const get_similar = async () => {
+
+        let similarResult = await fetch(`https://api.themoviedb.org/3/movie/${location.state.selecredMovieData.id}/similar?api_key=${process.env.REACT_APP_API_KEY}&page=${sCounter}`, {
+            method: "GET",
+            headers: {
+                'Content-type': 'application/json'
+            }
+        });
+
+        similarResult = await similarResult.json();
+
+        if (similarResult) {
+            sflag.current = false;
+            setSData(similarResult);
         }
     }
 
     useEffect(() => {
-        if (location.state && api_flag.current) {
-            get_review();
+        if (location.state) {
+            if (rflag.current) {
+                get_review();
+
+            }
+
+            if (sflag.current) {
+                get_similar();
+
+            }
+
+
         }
 
+
     });
+
+    useEffect(() => {
+        get_similar();
+    }, [sCounter]);
+
+    const handleSPage = (pageStatus) => {
+        if (sCounter == similarData.total_pages && pageStatus == false) {
+            return setSCounter(1);
+        }
+        else if (sCounter == 1 && pageStatus == true) {
+            return setSCounter(similarData.total_pages);
+        }
+
+        if (pageStatus) {
+            setSCounter(sCounter - 1);
+
+        }
+        else {
+            setSCounter(sCounter + 1);
+        }
+    }
 
     const UserReview = () => {
 
@@ -68,6 +119,63 @@ function Movie() {
                 <>
                     <span>
                         Getting user Reviews
+                    </span>
+                </>
+            );
+        }
+    };
+
+    const SimilarMovies = () => {
+        if (similarData) {
+            return (
+                <>
+                    <Container fluid={true} className={hstyles.card_group}>
+                        {similarData.results.map((movie, index) =>
+
+                            <div className={`card  ${hstyles.card_container}`} key={index} onClick={() => navigate("/movie", { state: { selecredMovieData: movie, genresData: location.state.genresData } })}>
+                                <div className={hstyles.card_poster}>
+                                    <img className={`img-fluid ${hstyles.card_img}`} src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="movie image" />
+                                </div>
+
+
+                                <div className={hstyles.main_text_div}>
+                                    <div className={hstyles.card_text_div}>
+                                        <span className={`${hstyles.card_text} ${hstyles.title}`}>{movie.title}</span>
+
+                                        <span className={`${hstyles.card_text} ${hstyles.genres_text}`}>
+                                            {location.state.genresData.genres ? movie.genre_ids.map((gen_id, index2) =>
+                                                <span key={index2}>
+                                                    | {location.state.genresData.genres.find(gen => gen.id == gen_id).name} |
+                                                </span>
+                                            )
+                                                : <span className={`${hstyles.card_text}`}>getting genres</span>}
+                                        </span>
+
+
+                                        <span className={`${hstyles.card_text}`}>{movie.release_date}</span>
+                                        <span className={`${hstyles.card_text} ${hstyles.label}`}>Overview</span>
+                                        <span className={`${hstyles.card_text} ${hstyles.overview}`}>{movie.overview}</span>
+                                    </div>                                </div>
+                            </div>
+                        )}
+                    </Container>
+                    <Container>
+
+                        <div className={hstyles.pagination_div}>
+
+                            <button className={hstyles.page_btn} onClick={() => handleSPage(true)}> Prev</button>
+                            <Page data={similarData} counter={sCounter}></Page>
+                            <button className={hstyles.page_btn} onClick={() => handleSPage(false)}>Next </button>
+                        </div>
+                    </Container>
+                </>
+            );
+        }
+        else {
+            return (
+                <>
+                    <span>
+                        Getting Similar Movies
                     </span>
                 </>
             );
@@ -115,20 +223,21 @@ function Movie() {
                             <UserReview></UserReview>
                         </Col>
                     </Row>
+
+                    <Row>
+                        <Col>
+                            <div className={styles.user_review}>
+                                <span >Similar Movies </span>
+                            </div>
+
+                            <SimilarMovies></SimilarMovies>
+                        </Col>
+                    </Row>
                 </Container>
             );
         }
         else {
-            // alert("here")
-            // return (<Container>
-            //     <Row>
-            //         <Col>
-            //             <span className={styles.redirect}>Redirecting you to home page </span>
-            //         </Col>
-            //     </Row>
 
-            // </Container>
-            //);
             return <Navigate to="/"></Navigate>
 
 
@@ -138,44 +247,8 @@ function Movie() {
 
     return (
         <CheckLocation></CheckLocation>
-        // <Container fluid={true}>
-        //     <Row>
-        //         <Col>
-        //             <div className={styles.main_div} >
-        //                 <div className={styles.poster_div} style={{ background: `url(https://image.tmdb.org/t/p/original/${location.state.selecredMovieData.poster_path}`, backgroundRepeat: "no-repeat", backgroundPosition: "center center", backgroundSize: "100% 100%" }}>
 
-        //                 </div>
 
-        //             </div>
-
-        //             <div className={styles.text_div}>
-        //                 <span className={`${styles.movie_text} ${styles.title}`}>{location.state.selecredMovieData.title}  </span>
-        //                 <span className={styles.movie_text}>{location.state.selecredMovieData.original_language}  </span>
-
-        //                 <span className={`${styles.movie_text} ${styles.genres_text}`}> Genres{location.state.genresData.genres ? location.state.selecredMovieData.genre_ids.map(gen_id =>
-        //                     <span className=""> | {location.state.genresData.genres.find(gen => gen.id == gen_id).name} | </span>
-        //                 )
-        //                     : <span className="">getting genres</span>}</span>
-        //                 <span className={styles.movie_text}>{location.state.selecredMovieData.overview}  </span>
-        //                 <span className={styles.movie_text}>{location.state.selecredMovieData.popularity}  </span>
-        //                 <span className={styles.movie_text}>Release Date {location.state.selecredMovieData.release_date}  </span>
-
-        //                 <span className={styles.movie_text}>{location.state.selecredMovieData.vote_count}  </span>
-        //                 <span className={styles.movie_text}>{location.state.selecredMovieData.vote_average}  </span>
-        //             </div>
-        //         </Col>
-        //     </Row>
-
-        //     <Row>
-        //         <Col>
-        //             <div className={styles.user_review}>
-        //                 <span >User Reviews </span>
-        //             </div>
-
-        //             <UserReview></UserReview>
-        //         </Col>
-        //     </Row>
-        // </Container>
     );
 }
 
